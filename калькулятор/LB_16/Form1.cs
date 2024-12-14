68855df0c55c18cad845bac03792581cebfad5a8
Taklerr <101276696+Taklerr@users.noreply.github.com>
2024-12-14 09:38:12 +0500

    Add files via upload

---
+++ калькулятор/LB_16/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace LB_16
+{
+    public partial class Form1 : Form
+    {
+        float a, b;
+        int count;
+        bool znak = true;
+        private void calculate()
+        {
+            switch (count)
+            {
+                case 1:
+                    b = a + float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 2:
+                    b = a - float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 3:
+                    b = a * float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 4:
+                    b = a / float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ".";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 4;
+            label1.Text = a.ToString() + "/";
+            znak = true;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 3;
+            label1.Text = a.ToString() + "*";
+            znak = true;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text); 
+            textBox1.Clear(); 
+            count = 2;
+            label1.Text = a.ToString() + "-";
+            znak = true;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 1;
+            label1.Text = a.ToString() + "+";
+            znak = true;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            string text1 = textBox1.Text;
+            double a = Convert.ToDouble(text1);
+            a = Math.Sqrt(a);
+            textBox1.Text = a.ToString();
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(textBox1.Text);
+            int result = number * number;
+            textBox1.Text = result.ToString();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(textBox1.Text);
+            int result = number * number * number;
+            textBox1.Text = result.ToString();
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(textBox1.Text);
+            long factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            textBox1.Text =  factorial.ToString();
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+
+            if (float.TryParse(input, out float number))
+            {
+                number = -number;
+                textBox1.Text = number.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Некорректный формат числа");
+            }
+           
+            count = 2;
+            znak = true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            calculate();
+            label1.Text = "";
+        }
+    }
+}
